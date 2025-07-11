e0b0b8f21c61376bc484e75df71838c74750dde7
lukechidubem <lukechidubem@gmail.com>
2025-07-11 08:32:15 +0100

    added test

--- test/MyTokenTest.t.sol
+++ test/MyTokenTest.t.sol
@@ -6,8 +6,6 @@ import {DeployMyToken} from "../script/DeployMyToken.s.sol";
 import {MyToken} from "../src/MyToken.sol";
 import {Test, console} from "forge-std/Test.sol";
 
-// import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
-
 interface MintableToken {
     function mint(address, uint256) external;
 }
@@ -19,4 +17,43 @@ contract OurTokenTest is Test {
     MyToken public myToken;
     DeployMyToken public deployer;
     address public deployerAddress;
+    address bob;
+    address alice;
+
+    function setUp() public {
+        deployer = new DeployMyToken();
+
+        myToken = new MyToken(INITIAL_SUPPLY);
+        myToken.transfer(msg.sender, INITIAL_SUPPLY);
+
+        bob = makeAddr("bob");
+        alice = makeAddr("alice");
+
+        vm.prank(msg.sender);
+        myToken.transfer(bob, BOB_STARTING_AMOUNT);
+    }
+
+    function testInitialSupply() public view {
+        assertEq(myToken.totalSupply(), deployer.INITIAL_SUPPLY());
+    }
+
+    function testUsersCantMint() public {
+        vm.expectRevert();
+        MintableToken(address(myToken)).mint(address(this), 1);
+    }
+
+    function testAllowances() public {
+        uint256 initialAllowance = 1000;
+
+        // Bob approves Alice to spend tokens on his behalf
+
+        vm.prank(bob);
+        myToken.approve(alice, initialAllowance);
+        uint256 transferAmount = 500;
+
+        vm.prank(alice);
+        myToken.transferFrom(bob, alice, transferAmount);
+        assertEq(myToken.balanceOf(alice), transferAmount);
+        assertEq(myToken.balanceOf(bob), BOB_STARTING_AMOUNT - transferAmount);
+    }
 }
